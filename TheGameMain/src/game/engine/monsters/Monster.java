1fd9516b9a4696bf02b1651e7da8e02f7e903b9a
The-Abdo <abdelrhman2006mohamed@gmail.com>
2026-03-06 17:55:44 +0200

    monsters1

--- TheGameMain/src/game/engine/monsters/Monster.java
+++ TheGameMain/src/game/engine/monsters/Monster.java
@@ -1,5 +1,32 @@
 package game.engine.monsters;
 
 public class Monster {
-
+	private String name;
+	private String description;
+	private Role role;
+	private Role originalRole;
+	private int energy;
+	private int position;
+	private boolean frozen;
+	private boolean shielded;
+	private int confusionTurns;
+	public Monster(String name, String description, Role originalRole, int energy)
+	{
+		this.name=name;
+		this.description=description;
+		this.originalRole = originalRole;
+		this.energy=energy;
+		position = 0;
+		confusionTurns=0;
+	}
+	int compareTo(Monster o)
+	{
+		if(this.position>o.position)
+			return 1;
+		else if(this.position<o.position)
+			return -1;
+		else
+			return 0;
+	}
+	
 }
