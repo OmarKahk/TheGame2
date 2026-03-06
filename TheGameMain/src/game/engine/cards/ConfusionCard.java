755f1e4430146d5e74c04056ee49bff10a1be0dc
OmarKahk <omarosamasaadmohamed@gmail.com>
2026-03-06 23:23:10 +0200

    All Card Classes created and edited, all steps done till 5.19 for now.

---
+++ TheGameMain/src/game/engine/cards/ConfusionCard.java
@@ -0,0 +1,11 @@
+package game.engine.cards;
+
+public class ConfusionCard extends Card {
+	
+	private int duration;
+	
+	public ConfusionCard(String name, String description, int rarity, int duration) {
+		super(name, description, rarity, false);
+		this.duration = duration;
+	}
+}
--- TheGameMain/src/game/engine/cells/CardCell.java
+++ TheGameMain/src/game/engine/cells/CardCell.java
@@ -4,6 +4,6 @@ public class CardCell extends Cell{
 
 	public CardCell(String name) {
 		super(name);
-		//ayhagadfsdsfdfssd
+		
 	}
 }
