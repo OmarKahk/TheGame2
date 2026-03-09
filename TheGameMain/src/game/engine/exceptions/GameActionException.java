0086e5bdbc1bcf09dd7acb4bdd8c810e864c683f
Daniel8982 <danydondon2006@gmail.com>
2026-03-09 18:58:55 +0200

    class

---
+++ TheGameMain/src/game/engine/exceptions/GameActionException.java
@@ -0,0 +1,15 @@
+package game.engine.exceptions;
+
+public abstract class GameActionException 
+{
+	
+	public GameActionException()
+	{
+		super();
+	}
+	public GameActionException(String message)
+	{
+		super();
+	}
+	
+}
---
+++ TheGameMain/src/game/engine/exceptions/InvalidMoveException.java
@@ -0,0 +1,11 @@
+package game.engine.exceptions;
+
+public class InvalidMoveException extends GameActionException
+{
+	private static final String MSG = "Invalid move attempted";
+	
+	public InvalidMoveException() 
+	{
+		
+	}
+}
