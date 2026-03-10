0f21e19b69389d98ae4ad42cd3838132eb7cc1c6
Abdelrahman-Adham-Almowafy <ab.almowafy@gmail.com>
2026-03-10 17:50:03 +0200

    Changes Constants (made em static) and added board class

--- TheGameMain/src/game/engine/Board.java
+++ TheGameMain/src/game/engine/Board.java
@@ -1,5 +1,47 @@
 package game.engine;
+import java.util.ArrayList;
+
+import game.engine.cards.Card;
+import game.engine.cells.Cell;
+import game.engine.monsters.Monster;
 
 public class Board {
+	private Cell[][] boardCells;
+	private ArrayList<Monster> stationedMonsters;
+	private ArrayList<Card> originalCards;
+	private ArrayList<Card> cards;
+	
+	public Board(ArrayList<Card> readCards) {
+		boardCells = new Cell[Constants.BOARD_ROWS][Constants.BOARD_COLS];
+		stationedMonsters = new ArrayList<>();
+		cards = new ArrayList<>();
+		originalCards = readCards;
+		
+	}
 
+	public ArrayList<Monster> getStationedMonsters() {
+		return stationedMonsters;
+	}
+
+	public void setStationedMonsters(ArrayList<Monster> stationedMonsters) {
+		this.stationedMonsters = stationedMonsters;
+	}
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+	public Cell[][] getBoardCells() {
+		return boardCells;
+	}
+
+	public ArrayList<Card> getOriginalCards() {
+		return originalCards;
+	}
+	
+	
 }
