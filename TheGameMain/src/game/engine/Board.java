effcda29ebe632e19d77f7e4e70bfd0ba0162187
The-Abdo <abdelrhman2006mohamed@gmail.com>
2026-03-11 08:04:45 +0200

    abdo

--- TheGameMain/src/game/engine/Board.java
+++ TheGameMain/src/game/engine/Board.java
@@ -1,5 +1,23 @@
 package game.engine;
 
+import java.util.ArrayList;
+import game.engine.*;
+import game.engine.cards.Card;
+import game.engine.cells.Cell;
+import game.engine.monsters.*;
 public class Board {
-
+	private Cell[][] boardCells;
+	ArrayList<Monster> stationedMonsters;
+	ArrayList<Card> originalCards;
+	ArrayList<Card> cards;
+	Board(ArrayList<Card> readCards)
+	{
+		int x = Constants.BOARD_ROWS;
+		
+		boardCells = new Cell[BOARD_ROWS][BOARD_COLS] ;
+		stationedMonsters = new ArrayList();
+		cards = new ArrayList();
+		originalCards = 
+	}
+	
 }
