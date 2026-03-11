d9d3afdb372c1c521f00872819d48a88af94d638
The-Abdo <abdelrhman2006mohamed@gmail.com>
2026-03-11 09:47:18 +0200

    pp

--- TheGameMain/src/game/engine/Game.java
+++ TheGameMain/src/game/engine/Game.java
@@ -1,5 +1,17 @@
 package game.engine;
 
-public class Game {
+import java.util.ArrayList;
 
+import game.engine.monsters.Monster;
+
+public class Game {
+private Board board;
+private ArrayList<Monster> allMonsers;
+private Monster player;
+private Monster current;
+	/*Game(Role playerRole) throws IOExeptions
+	{
+		board = new Board();
+		
+	}*/
 }
