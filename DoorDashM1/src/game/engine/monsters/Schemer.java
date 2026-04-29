23bd39003dcd1761b2f58483dee398bcb6406f54
OmarKahk <omarosamasaadmohamed@gmail.com>
2026-04-29 22:49:50 +0300

    Revert "Merge branch 'master' of https://github.com/OmarKahk/TheGame2"
    This reverts commit 00d217616291ad692e99d3b4605b1e4edfe3c0b3, reversing
    changes made to 76637f70a0480761d38ca15d05ad9913813d0ffb.

--- DoorDashM1/src/game/engine/monsters/Schemer.java
+++ DoorDashM1/src/game/engine/monsters/Schemer.java
@@ -32,16 +32,12 @@ public class Schemer extends Monster {
 	 
 	 public void executePowerupEffect(Monster opponentMonster)
 	 {
-		 
-		
-			int total = 0;
+		 int total = 0;
 		 total += stealEnergyFrom(opponentMonster);
 		 for(int i = 0; i<Board.getStationedMonsters().size();i++)
 		 {
 			 total += stealEnergyFrom((Monster)Board.getStationedMonsters().get(i));
 		 }
 		 this.alterEnergy(total);
-		 
-		
-	}
+	 }
 }
